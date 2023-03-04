c86bb41190dadccacd4fb86a706a835da4718dba
Alex Sandro <alexsccpead@gmail.com>
2023-03-04 18:07:41 -0300

    Lógica da seleção das peças

--- Program.cs
+++ Program.cs
@@ -1,116 +1,141 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 
 namespace domino_estrutura_de_dados
 {
     class Program
     {
+        class Peca
+        {
+            public byte ladoA = 0;
+            public byte ladoB = 0;
+            public string valores;
+
+            public Peca(byte ladoA, byte ladoB)
+            {
+                this.ladoA = ladoA;
+                this.ladoB = ladoB;
+                this.valores = ($"| {ladoA} | {ladoB} |");
+            }
+        }
+
+        class PecasJogadas
+        {
+            public string linha1 = "---------";
+            public List<string> linha21 = new List<string>();
+            public List<string> linha22 = new List<string>();
+            public List<string> linha23 = new List<string>();
+            public List<string> linha24 = new List<string>();
+
+            public void Adicionar(Peca peca)
+            {
+                if (linha21.Count < 7)
+                {
+                    linha21.Add($"{peca.valores}");
+                }
+                else if (linha22.Count < 7)
+                {
+                    linha22.Add($"{peca.valores}");
+                }
+                else if (linha23.Count < 7)
+                {
+                    linha23.Add($"{peca.valores}");
+                }
+                else if (linha24.Count < 7)
+                {
+                    linha24.Add($"{peca.valores}");
+                }
+            }
+
+            public void Linhas(int x)
+            {
+                for (byte i = 0; i < x; i++)
+                {
+                    Console.Write($"{linha1} ");
+                }
+
+            }
+
+            public void LinhaPecas(List<string> x)
+            {
+                Linhas(x.Count);
+                Console.WriteLine();
+                x.ForEach(item => Console.Write($"{item} "));
+                Console.WriteLine();
+                Linhas(x.Count);
+                Console.WriteLine();
+            }
+            public void Mostrar()
+            {
+                LinhaPecas(linha21);
+
+                if (linha22.Count > 0)
+                {
+                    LinhaPecas(linha22);
+                }
+
+                if (linha23.Count > 0)
+                {
+                    LinhaPecas(linha23);
+                }
+
+                if (linha24.Count > 0)
+                {
+                    LinhaPecas(linha24);
+                }
+
+            }
+        }
+
+        static List<string> pecas = new List<string>() { "00", "01", "11", "02", "12", "22", "03", "13", "23", "33", "04", "14", "24", "34", "44", "05", "15", "25", "35", "45", "55", "06", "16", "26", "36", "46", "56", "66" };
         static void Main(string[] args)
         {
-            Console.WriteLine("");
-            string[] pecas = {"0 | 0", "0 | 1", "1 | 1", "0 | 2", "1 | 2", "2 | 2", "0 | 3", "1 | 3", "2 | 3", "3 | 3", "0 | 4", "1 | 4", "2 | 4", "3 | 4", "4 | 4", "0 | 5", "1 | 5", "2 | 5", "3 | 5", "4 | 5", "5 | 5", "0 | 6", "1 | 6", "2 | 6", "3 | 6", "4 | 6", "5 | 6", "6 | 6"};
+            // PecasJogadas mesa = new PecasJogadas();
+            // Peca peca1 = new Peca(2, 2);
+            // mesa.Adicionar(peca1);
 
-            //Random random = new Random();
-            ConsoleColor corDetalhes, corPeca;
-            string opcCorPeca, opcCorNumeros;
-
-            Console.WriteLine("Escolha a cor das peças: ");
-            Console.WriteLine("1 - BRANCO");
-            Console.WriteLine("2 - PRETO");
-            while (true)
-            {
-                Console.Write(": ");
-                opcCorPeca = Console.ReadLine().Trim();
-                if (opcCorPeca == "1")
-                {
-                    corPeca = ConsoleColor.White;
-                    break;
-                }
-                else if (opcCorPeca == "2")
-                {
-                    corPeca = ConsoleColor.Black;
-                    break;
-                }
-
-                else
-                {
-                    Console.WriteLine("Opção inválida! Por favor, tente novamente.");
-                }
-            }
-
-
-            Console.WriteLine("Escolha a cor dos números: ");
-            Console.WriteLine("1 - VERMELHO");
-            Console.WriteLine("2 - AZUL");
-            Console.WriteLine("3 - AMARELO");
-            Console.WriteLine("4 - VERDE");
-            if (opcCorPeca == "1")
-            {
-                Console.WriteLine("5 - PRETO");
-            }
-            else
-            {
-                Console.WriteLine("5 - BRANCO");
-            }
-            while (true)
-            {
-                Console.Write(": ");
-                opcCorNumeros = Console.ReadLine().Trim();
-                if (opcCorNumeros == "1")
-                {
-                    corDetalhes = ConsoleColor.Red;
-                    break;
-                }
-                else if (opcCorNumeros == "2")
-                {
-                    corDetalhes = ConsoleColor.Blue;
-                    break;
-                }
-                else if (opcCorNumeros == "3")
-                {
-                    corDetalhes = ConsoleColor.Yellow;
-                    break;
-                }
-                else if (opcCorNumeros == "4")
-                {
-                    corDetalhes = ConsoleColor.Green;
-                    break;
-                }
-                else if (opcCorNumeros == "5")
-                {
-                    if (opcCorPeca == "1")
-                    {
-                        corDetalhes = ConsoleColor.Black;
-                    }
-                    else
-                    {
-                        corDetalhes = ConsoleColor.White;
-                    }
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Opção inválida! Por favor, tente novamente.");
-                }
-            }
-
-
-            for (int i = 0; i < pecas.Length; i++)
-            {
-                Console.ForegroundColor = corDetalhes;
-                Console.BackgroundColor = corPeca;
-                if (i > 0 && i % 7 == 0)
-                {
-                    Console.WriteLine();
-                }
-
-                Console.Write($"{pecas[i]}");
-                Console.ResetColor();
-                Console.Write(" ");
-            }
+            //gerar os jogos
+            List<string> j1 = NovoJogoP();
+            j1.ForEach(item => Console.WriteLine(item));
             Console.WriteLine();
 
-            //Console.Clear();
+            List<string> j2 = NovoJogoC(j1);
+            j2.ForEach(item => Console.WriteLine(item));
+            Console.WriteLine();
+
+
+
+        }
+
+        public static List<string> NovoJogoP()
+        {
+            List<string> jogoP = new List<string>();
+            while (jogoP.Count < 7)
+            {
+                Random random = new Random();
+                string peca = pecas[random.Next(0, 28)];
+                if (!jogoP.Contains(peca))
+                {
+                    jogoP.Add(peca);
+                }
+            }
+            return jogoP;
+        }
+
+        public static List<string> NovoJogoC(List<string> jogoP)
+        {
+            List<string> jogoC = new List<string>();
+            while (jogoC.Count < 7)
+            {
+                Random random = new Random();
+                string peca = pecas[random.Next(0, 28)];
+                if (!jogoP.Contains(peca) && !jogoC.Contains(peca))
+                {
+                    jogoC.Add(peca);
+                }     
+            }
+            return jogoC;
         }
     }
 }
